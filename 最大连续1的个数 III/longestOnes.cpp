523ffcbaff4c96bdd1cafa8856c96b61056796cd
pan102887 <pan102887@qq.com>
2021-02-20 01:02:57 +0800

    滑动窗口-最大连续1的个数

--- 最大连续1的个数 III/longestOnes.cpp
+++ 最大连续1的个数 III/longestOnes.cpp
@@ -22,6 +22,23 @@ public:
         }
         return ans;
     }
+    int longestOnes_2(vector<int> &A, int K)
+    {
+        int n = A.size();
+        int left = 0, lsum = 0, rsum = 0;
+        int ans = 0;
+        for (int right = 0; right < n; ++right)
+        {
+            rsum += 1 - A[right];
+            while (lsum < rsum - K)
+            {
+                lsum += 1 - A[left];
+                ++left;
+            }
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
 };
 int main(int argc, char **argv)
 {
